52cf1ae7c6ceb06dae55466a6d2ec224917af52d
Tyrone Onyebuagu <tyrone.onyebuagu@gmail.com>
2023-10-13 14:52:09 +0100

    removed some scroll behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,20 @@ import OurAdvantages from "./components/OurAdvantages/OurAdvantages";
 import Header from "./components/Header/Header";
 import Revolution from "./components/Revolutionizing/Revolution";
 import Explore from "./components/Explore/Explore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { useWindowSize } from "utils/useWindowSize/useWindowSize";
 import useDisclosure from "utils/useDisclosure/useDisclosure";
 
-
-
 function App() {
   const style = useWindowSize();
   const view = useDisclosure();
+  const [activeTime, setActiveTime] = useState<number>(0);
 
   useEffect(() => {
-    setTimeout(view.onOpen, 5000)
-  }, [])
+    setTimeout(view.onOpen, 5000);
+    window.scrollTo({top: 200, behavior: 'smooth'})
+  }, []);
 
   return (
     <main style={style} className="overflow-scroll scrollbar-hide">
@@ -28,28 +28,38 @@ function App() {
       ) : (
         <>
           <Header />
-          <Section id={1} content={<OurAdvantages />} />
-          <Section id={2} content={<Revolution  />} />
-          <Section id={3} content={<Explore />} />
+          <Section
+            id={1}
+            content={<OurAdvantages />}
+          />
+          <Section
+            id={2}
+            content={<Revolution />}
+          />
+          <Section
+            id={3}
+            content={<Explore />}
+          />
         </>
       )}
     </main>
   );
 }
 
-const Section = ({ content, id=1 }) => {
+const Section = ({ content, id = 1, }) => {
   const style = useWindowSize();
   const view = useDisclosure(true);
   const { ref, inView, entry } = useInView();
 
   useEffect(() => {
+    console.log({ id, inView, entry });
     if (inView ) {
-      entry.target.scrollIntoView({ behavior: "smooth", block: 'start' });
-      view.onOpen()
+      // window.scrollTo({ behavior: "smooth", top:window.scrollY + entry?.boundingClientRect?.y ?? 0 });
+      view.onOpen();
     } else {
       setTimeout(view.onClose, 10);
     }
-  }, [inView, entry]);
+  }, [inView]);
 
   return (
     <div style={style} ref={ref} className="mb-4 overflow-hidden">
